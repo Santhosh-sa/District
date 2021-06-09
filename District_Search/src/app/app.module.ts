62215a2b1a2a18e1f547ab2875b2dfe43e3dd30b
Santhosh-sa <79300066+Santhosh-sa@users.noreply.github.com>
2021-06-09 19:24:26 +0530

    Add files via upload

---
+++ District_Search/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BackgroundComponent } from './background/background.component';
+//import { ImageViewerComponent } from './components/image-viewer/image-viewer.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    BackgroundComponent,
+    
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
